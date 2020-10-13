32497b2a2c8906ffc37e6de2f7198f7fb3977cdc
Christian <c.mertenskoetter@live.com>
2020-10-13 13:35:46 +0200

    Cap Example

---
+++ examples/cap/module.js
@@ -0,0 +1,52 @@
+/** LCARS SDK 19182.4
+* This file is a part of the LCARS SDK.
+* https://github.com/AricwithanA/LCARS-SDK/blob/master/LICENSE.md
+* For more information please go to https://github.com/crstmkt/LCARS-SDK/wiki.
+**/
+
+//To render JS Code into DOM
+$( document ).ready(function() {
+    $('body').append( LCARS.create(nemesisUI).dom );
+});
+
+//Colours
+var uiColors = ['bg-blue-1', 'bg-blue-2','bg-blue-3','bg-blue-4','bg-green-1','bg-green-2','bg-green-3','bg-green-4'];
+
+//All this will be rendered into DOM
+var nemesisUI = {type:'wrapper', id:'wpr_viewport', version:'row', flex:'h', 
+arrive:function(){var dom = this.dom; LCARS.helper.viewportZoom(dom, {width:1440, height:1080}); 
+    window.addEventListener("resize", function(){LCARS.helper.viewportZoom(dom, {width:1440, height:1080});});},
+
+//All DOM CHildren for nemesisUI
+children:[
+      
+    //Main Area
+    {type:'wrapper', version:'column', id:'wpr_mainView', flex:'v', flexc:'h', children:[   
+    
+        {type:'row', version:'frame', flex:'h', children:[ //This is for horizontal alignment
+            {type:'cap', color:'bg-blue-1', version:'round-left', size:'tiny'},
+            {type:'cap', color:'bg-blue-1', version:'round', size:'tiny'},
+            {type:'cap', color:'bg-blue-1', version:'round-right', size:'tiny'}
+        ]},
+        {type:'row', version:'frame', flex:'h', children:[
+            {type:'cap', color:'bg-blue-1', version:'round-left', size:'small'},
+            {type:'cap', color:'bg-blue-1', version:'round', size:'small'},
+            {type:'cap', color:'bg-blue-1', version:'round-right', size:'small'}
+        ]},
+        {type:'row', version:'frame', flex:'h', children:[
+            {type:'cap', color:'bg-blue-1', version:'round-left', size:'medium'},
+            {type:'cap', color:'bg-blue-1', version:'round', size:'medium'},
+            {type:'cap', color:'bg-blue-1', version:'round-right', size:'medium'}
+        ]},
+        {type:'row', version:'frame', flex:'h', children:[
+            {type:'cap', color:'bg-blue-1', version:'round-left', size:'large'},
+            {type:'cap', color:'bg-blue-1', version:'round', size:'large'},
+            {type:'cap', color:'bg-blue-1', version:'round-right', size:'large'}
+        ]},
+        {type:'row', version:'frame', flex:'h', children:[
+            {type:'cap', color:'bg-blue-1', version:'round-left', size:'xlarge'},
+            {type:'cap', color:'bg-blue-1', version:'round', size:'xlarge'},
+            {type:'cap', color:'bg-blue-1', version:'round-right', size:'xlarge'}
+        ]}    
+    ]}
+]}; 
